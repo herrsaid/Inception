8e4116f075b43e0177a7dc03c8487796186124e7
said lamane <thisfor1337@gmail.com>
2022-12-13 10:09:59 -0500

    all

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -3,5 +3,8 @@
 wp config create --dbhost=mariadb \
     --dbname="wp" --dbuser="salamane" \
     --dbpass="Lamane@mariadb"  --allow-root
-
+wp core install --allow-root --url=salamane.42.fr \
+    --title=Website --admin_user=salamane \
+    --admin_password="strongpassword" \
+    --admin_email=saidbm3w@gmail.com
 php-fpm7.3 -F -R
