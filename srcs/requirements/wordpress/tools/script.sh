ba34740763622298c8eaf6728ab247523724646c
said lamane <thisfor1337@gmail.com>
2023-01-13 07:46:21 -0500

    all

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+wp core download --allow-root \
+     && chown -R www-data:www-data /var/www/html
 
 wp config create --dbhost=mariadb \
-    --dbname="wp" --dbuser="salamane" \
-    --dbpass="Lamane@mariadb"  --allow-root
+    --dbname=${DB_NAME} --dbuser=${DB_USER} \
+    --dbpass=${DB_PASSWORD}  --allow-root
+
 wp core install --allow-root --url=10.12.177.80 \
-    --title=Website --admin_user=salamane \
-    --admin_password="strongpassword" \
+    --title=Website --admin_user=${WP_ADMIN_USER} \
+    --admin_password=${WP_ADMIN_PASSWORD} \
     --admin_email=saidbm3w@gmail.com
 
 wp plugin install redis-cache --activate --allow-root
